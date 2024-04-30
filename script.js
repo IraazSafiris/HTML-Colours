3596bfa9543f2adbbe533ce51467372b5317b983
IraazSafiris <165715842+IraazSafiris@users.noreply.github.com>
2024-04-30 11:36:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function save(){
+  let email =
+document.getElementById("email").value
+  let password =
+document.getElementById("pwd").value
+document.getElementById("demo").innerHTML = "You have successfully registered!", email.value, "bla", pwd.value;
+document.getElementById("mail").innerHTML = email;
+document.getElementById("pass").innerHTML = password;
+}
